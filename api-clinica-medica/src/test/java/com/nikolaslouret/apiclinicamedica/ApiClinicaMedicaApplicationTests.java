8cad534e9a678cf94ae1af588865b19b7c052d2e
Nikolas Louret <nikoul.ret@gmail.com>
2024-05-03 18:30:05 -0300

    chore: initial project config

---
+++ api-clinica-medica/src/test/java/com/nikolaslouret/apiclinicamedica/ApiClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolaslouret.apiclinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
